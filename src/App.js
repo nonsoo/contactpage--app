b79f51c8075fc7f1a616c28d6ac6d77a6586b63b
Nonso <nonsootoh@gmail.com>
2021-08-31 20:37:17 -0400

    made custom card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./css/layout.css";
 
-// import data from "./data.json";
+import data from "./data.json";
+import Card from "./Components/Card";
 
 function App() {
-  // const dataHolder = data.data;
+  const dataHolder = data.data;
   return (
     <div className="App">
-      <p>hellow world</p>
+      <h1>Meet the Team</h1>
+      <div className="cardContainer">
+        {dataHolder.map((user) => (
+          <Card
+            profPic={user.avatar}
+            fName={user.first_name}
+            lName={user.last_name}
+            email={user.email}
+            position={user.position}
+          />
+        ))}
+      </div>
     </div>
   );
 }
